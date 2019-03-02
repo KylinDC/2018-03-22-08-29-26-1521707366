5e80780d7fe9f10f631f7d7da185630b16cdf9fa
KylinDC <inlovingdays@gmail.com>
2019-03-03 00:03:56 +0800

    finish the exercise of filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,31 +1,47 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int element : array) {
+            if (element % 2 == 0) {
+                result.add(element);
+            }
+        }
+        return result;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int element : array) {
+            if (element % 3 == 0) {
+                result.add(element);
+            }
+        }
+        return result;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> commons = new ArrayList<>();
+        for (int element : firstList) {
+            if (secondList.contains(element)) {
+                commons.add(element);
+            }
+        }
+        return commons;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        Set<Integer> evensSet = new HashSet<>(array);
+        return new ArrayList<Integer>(evensSet);
     }
 }
