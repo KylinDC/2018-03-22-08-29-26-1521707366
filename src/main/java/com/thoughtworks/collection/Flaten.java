4420dbc2e4c133bbb79edd7f9f8810d845028520
KylinDC <inlovingdays@gmail.com>
2019-03-03 00:04:22 +0800

    finish the exercise of flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,35 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer[] element : array) {
+            for (Integer i : element) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer[] element : array) {
+            for (Integer i : element) {
+                result.add(i);
+            }
+        }
+        return new ArrayList<Integer>(new LinkedHashSet<Integer>(result));
     }
 }
