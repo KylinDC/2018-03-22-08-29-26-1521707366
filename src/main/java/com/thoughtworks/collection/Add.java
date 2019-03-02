3a63c0703af7e452c647ab777cdec45b74e2e69e
KylinDC <inlovingdays@gmail.com>
2019-03-02 23:54:54 +0800

    finish exercise of add

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -1,51 +1,150 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.List;
+import java.util.*;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if (rightBorder < leftBorder) {
+            int temp = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = temp;
+        }
+        int startBorder = leftBorder % 2 == 0 ? leftBorder : leftBorder + 1;
+        int sum = 0;
+        for (int i = startBorder; i <= rightBorder; i += 2) {
+            sum += i;
+        }
+        return sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if (rightBorder < leftBorder) {
+            int temp = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = temp;
+        }
+        int startBorder = leftBorder % 2 != 0 ? leftBorder : leftBorder + 1;
+        int sum = 0;
+        for (int i = startBorder; i <= rightBorder; i += 2) {
+            sum += i;
+        }
+        return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (int element : arrayList) {
+            sum += element * 3 + 2;
+        }
+        return sum;
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 != 0) {
+                result.add(element * 3 + 2);
+            } else {
+                result.add(element);
+            }
+        }
+        return result;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (int element : arrayList) {
+            if (element % 2 != 0) {
+                sum += element * 3 + 5;
+            }
+        }
+        return sum;
     }
 
     public double getMedianOfEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> evens = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 == 0) {
+                evens.add(element);
+            }
+        }
+        Collections.sort(evens);
+        float middenIndex = (float) evens.size() / 2;
+        int middenLeft = evens.get((int) Math.ceil(middenIndex) - 1);
+        int middenRight = evens.get((int) Math.floor(middenIndex));
+        return (double) (middenLeft + middenRight) / 2;
     }
 
     public double getAverageOfEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> evens = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 == 0) {
+                evens.add(element);
+            }
+        }
+        int sumOfEvens = 0;
+        for (int element : evens) {
+            sumOfEvens += element;
+        }
+        return (double) sumOfEvens / evens.size();
     }
 
-    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
+    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElement) {
+        List<Integer> evens = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 == 0) {
+                evens.add(element);
+            }
+        }
+        return evens.contains(specialElement);
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> evens = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 == 0) {
+                evens.add(element);
+            }
+        }
+        HashSet<Integer> evensSet = new HashSet<Integer>(evens);
+        return new ArrayList<Integer>(evensSet);
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> evens = new ArrayList<>();
+        List<Integer> odds = new ArrayList<>();
+        for (int element : arrayList) {
+            if (element % 2 == 0) {
+                evens.add(element);
+            } else {
+                odds.add(element);
+            }
+        }
+
+        Comparator descendingSort =
+                new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        if (o2 > o1) {
+                            return 1;
+                        }
+                        return -1;
+                    }
+                };
+
+        List<Integer> result = new ArrayList<>();
+        Collections.sort(evens);
+        Collections.sort(odds, descendingSort);
+        result.addAll(evens);
+        result.addAll(odds);
+        return result;
     }
 
     public List<Integer> getProcessedList(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            result.add((arrayList.get(i) + arrayList.get(i + 1)) * 3);
+        }
+        return result;
     }
 }
